99926193fdc23a9a0e2b8eb1295dc730f7bb16ae
vitorSiquera <vitor.siqueira.simeao@gmail.com>
2025-08-15 10:24:19 -0300

    (Tasks):add delete tasks

--- src/App.jsx
+++ src/App.jsx
@@ -33,13 +33,18 @@ function onTaskClick(taskId) {
   setTasks(newTasks);
 }
 
+function onDeleteTaskClick(taskId) {
+  const newTasks = tasks.filter((task) => task.id !== taskId);
+  setTasks(newTasks);
+}
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciado de Tarefas
         </h1>
-        <Task tasks={tasks} onTaskClick={onTaskClick} />
+        <Task tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} />
       </div>
     </div>
   )
