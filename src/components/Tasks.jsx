f498f084b9df31734bc2d09103e0ee2f2aa9c1ab
vitorSiquera <vitor.siqueira.simeao@gmail.com>
2025-08-15 10:11:15 -0300

    (Tasks):adding task listing and completion

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,23 @@
-function Task({ task }) {
-  return (
-    <div>
-      <h3>{task.name}</h3>
-      <p>{task.description}</p>
-    </div>
+import { ChevronRightIcon } from "lucide-react";
+
+function Tasks( props ) {
+  return(
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+           <button 
+           onClick={() => props.onTaskClick(task.id)}
+           className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted ? "line-through" : ""}`}>
+            {task.title}
+            </button>
+           <button className="bg-slate-400 p-2 rounded-md text-white">            
+            <ChevronRightIcon/>            
+           </button>
+        </li>
+      
+      ))}
+
+    </ul>
   );
 }
- export default Task;   
+ export default Tasks;   
